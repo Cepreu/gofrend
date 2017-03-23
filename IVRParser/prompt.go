82c8fff6c73c79e884281d7c802f2094c25d07fa
Sergei Krupnov <skrupnov@gmail.com>
2017-03-22 22:42:10 -0700

    MultiPrompts added

--- IVRParser/prompt.go
+++ IVRParser/prompt.go
@@ -32,3 +32,19 @@ type VivrHeader struct {
 type VivrPrompt struct {
 	VivrXML string `xml:"xml"`
 }
+
+type multiLanguagesPrompts struct {
+	Entries []MLPromptEntry `xml:"entry"`
+}
+type MLPromptEntry struct {
+	Key string `xml:"key"`
+	Id string `xml:"value>promptId"`
+	Name string `xml:"value>name"`
+	Description string `xml:"value>description"`
+	Type string `xml:"value>type"`
+	Prompts MLPrompts `xml:"prompts"`
+	DefLanguage string `xml:"defaultLanguage"`
+	isPersistent bool `xml:"isPersistent"`
+}
+type MLPrompts struct {
+	Entries []MLEntry `xml:"entry"`
