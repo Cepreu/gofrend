1ba65c976779ea29b739cc4b55be10b676cbe6ba
Sergei Krupnov <skrupnov@gmail.com>
2017-03-29 00:02:57 -0700

    more tests...

--- variables/value_test.go
+++ variables/value_test.go
@@ -16,3 +16,14 @@ func TestInteger(t *testing.T) {
 		t.Error("Expected 1234", "got ", strIv)
 	}
 }
+func TestCompare(t  *testing.T) {
+	iv1 := IntegerValue{false, 1234}
+	compArray := [...][3]int {{0, 1234, 0}, {1, 1234, 0}, {0, 2000, -1}, {0, 1000, 1}}
+	for _, turple := range compArray {
+		ivI := IntegerValue{1==turple[0], int64(turple[1])}
+		res, _ := iv1.compareTo(&ivI)
+		if res != turple[2] {
+			t.Error("Expected ", turple[2],  ", got ", res)
+		}
+	}
+}
