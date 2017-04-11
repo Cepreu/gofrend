f5ac17c82ba930a261b771e1a6149b86a5edf71c
Cepreu <skrupnov@gmail.com>
2017-04-11 12:31:41 -0700

    datevalue - array

--- variables/dateValue.go
+++ variables/dateValue.go
@@ -5,11 +5,15 @@ import (
 	"fmt"
 )
 
+const (
+	year int = iota
+	month
+	day
+)
+
 type DateValue struct {
 	defaultValueImpl
-	day   int
-	month int
-	year  int
+	value [3]int
 }
 
 func (dval *DateValue) isSecure() bool { return dval.secure }
@@ -18,9 +22,9 @@ func (dval *DateValue) assign(that Value) error {
 	dval.defaultValueImpl.assign(that)
 	v, err := that.toDate()
 	if err == nil {
-		dval.day = v[2]
-		dval.month = v[1]
-		dval.year = v[0]
+		dval.value[day] = v[day]
+		dval.value[month] = v[month]
+		dval.value[year] = v[year]
 	}
 	return err
 }
@@ -28,18 +32,17 @@ func (dval *DateValue) assign(that Value) error {
 func (dval *DateValue) new(secure bool, strValue string) error {
 	dval.secure = secure
 	var err error
-	dval.day, dval.month, dval.year, err = vuStringToDate(strValue)
+	dval.value[day], dval.value[month], dval.value[year], err = vuStringToDate(strValue)
 	return err
 }
 
 func (dval *DateValue) compareTo(value2 Value) (int, error) {
 	res := 0
-	toCompare, err := value2.toDate()
-	if err != nil {
+	if value2.getType() != DATE {
 		return res, errors.New("Variable to compare must be of DateValue type")
 	}
-	d1 := dval.day + dval.month*100 + dval.year*10000
-	d2 := toCompare[2] + toCompare[1]*100 + toCompare[0]*10000
+	d1, _ := dval.toLong()
+	d2, _ := value2.toLong()
 	if d1 > d2 {
 		res = 1
 	} else if d1 < d2 {
@@ -49,7 +52,7 @@ func (dval *DateValue) compareTo(value2 Value) (int, error) {
 }
 
 func (dval *DateValue) toLong() (int64, error) {
-	return int64(dval.day + dval.month*100 + dval.year*10000), nil
+	return int64(dval.value[day] + dval.value[month]*100 + dval.value[year]*10000), nil
 }
 
 func (dval *DateValue) toString() string {
@@ -61,11 +64,11 @@ func (dval *DateValue) toString() string {
 }
 
 func (dval *DateValue) convertToString() (string, error) {
-	return fmt.Sprintf("%4d-%02d-%02d", dval.year, dval.month, dval.day), nil
+	return fmt.Sprintf("%4d-%02d-%02d", dval.value[year], dval.value[month], dval.value[day]), nil
 }
 
 func (dval *DateValue) toDate() ([]int, error) {
-	return []int{dval.year, dval.month, dval.day}, nil
+	return dval.value[:], nil
 }
 
 func (*DateValue) getType() Type {
