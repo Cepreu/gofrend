afdc7230adeceabe92a256f4ac78d25eda811e75
Matthew Khudari <matthew.khudari@five9.com>
2019-06-20 10:25:03 -0700

    Bugfix: change vars.Integer methods to accept pointer

--- fulfiller/interpreter_test.go
+++ fulfiller/interpreter_test.go
@@ -1,11 +1,5 @@
 package fulfiller
 
-import (
-	"testing"
-
-	dialogflowpb "google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
-)
-
-func TestInterpret(t *testing.T) {
-	webhookRequest := &dialogflowpb.WebhookRequest{}
-}
+// func TestInterpret(t *testing.T) {
+// 	webhookRequest := &dialogflowpb.WebhookRequest{}
+// }
