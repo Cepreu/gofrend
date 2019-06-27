6264b6023683d43c61f5c1bf02a1c4b908c2bf0d
Matthew Khudari <matthew.khudari@five9.com>
2019-06-27 15:02:10 -0700

    Remove padding from base64 encoding

--- utils/hash.go
+++ utils/hash.go
@@ -8,5 +8,6 @@ import (
 // HashToString returns the sha256 hash of a byte slice in string format
 func HashToString(data []byte) string {
 	hashbytes := sha256.Sum256(data)
-	return base64.URLEncoding.EncodeToString(hashbytes[:])
+	encoding := base64.URLEncoding.WithPadding(base64.NoPadding)
+	return encoding.EncodeToString(hashbytes[:])
 }
